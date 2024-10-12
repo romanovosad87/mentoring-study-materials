b248b88624688e2b14730302d81e646cf6acaac6
romanovosad87 <roman.novosad@yahoo.com>
2024-10-12 02:29:26 +0200

    myThenComparing()

--- fundamentals/src/main/java/functional/DemoApp.java
+++ fundamentals/src/main/java/functional/DemoApp.java
@@ -6,9 +6,17 @@ import java.util.function.Function;
 
 public class DemoApp {
     public static void main(String[] args) {
-        Account oldest = Account.generate(10).stream()
-            .max(myComparing(Account::getAge))
-            .orElseThrow();
-        System.out.println(oldest);
+        Account oldestAndLongestEmail = Account.generate(10).stream()
+                .peek(System.out::println)
+                .max(myThenComparing(myComparing(Account::getAge), Account::getEmail))
+                .orElseThrow();
+        System.out.println("-----------");
+        System.out.println(oldestAndLongestEmail);
     }
+
+    private static <T, R extends Comparable<? super R>> Comparator<T> myComparing(
+            Function<? super T, ? extends R> function) {
+        return (t1, t2) -> function.apply(t1).compareTo(function.apply(t2));
+    }
+
 }
