98044ce5749ae54764ce2e400937a04048db76cb
romanovosad87 <roman.novosad@yahoo.com>
2024-10-27 15:29:52 +0100

    web-intro

---
+++ web/demo/src/main/java/com/example/demo/controller/mapper/StudentMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller.mapper;
+
+import com.example.demo.controller.RequestDto;
+import com.example.demo.model.Student;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentMapper {
+
+    public Student toStudent(RequestDto dto) {
+        Student student = new Student();
+        student.setName(dto.name());
+        student.setMood(dto.mood());
+        return student;
+    }
+}
---
+++ web/demo/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String mood;
+}
